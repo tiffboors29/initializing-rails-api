cb0b641b889496dc565d85cb178edd98bf2f82db
tiffboors29 <tiffanyboorsma@gmail.com>
2015-07-24 14:19:27 -0400

    Add User.login returning token for authentication
    Remove privated from set_token until access/permission issues worked out.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,17 @@
 class User < ActiveRecord::Base
   has_secure_password
+
   before_create :set_token
 
   validates :email, uniqueness: true
 
-  private
+  def self.login(email, password)
+    user = find_by email: email
+    user = user.authenticate password if user
+    user.set_token && user.save! if user
+    user.token if user
+  end
+
 
   def set_token
     self.token = SecureRandom.hex
