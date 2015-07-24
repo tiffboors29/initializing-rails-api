3daf9951bcfe32c7dfddde474887817a9b5018f6
tiffboors29 <tiffanyboorsma@gmail.com>
2015-07-24 12:02:31 -0400

    Add constraints and seed users
    Added validates uniqueness to User model.
    Added unique constraints to user email and not null to both user
    email and password_digest.
    Added users to the db/seeds.rb for testing.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+%w(and dna dan).each do |name|  # use strings to create users
+  email = "#{name}@#{name}.com"
+  next if User.exists? email: email # next means skip to next iteration
+  User.create!(email: email, password: 'abc123', password_confirmation: 'abc123')
+end
